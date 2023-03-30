7268cfcc454fb9b8fb5f9c17933acbd62b634776
Diego Aguado <diego.aguado@bosonit.com>
2023-03-30 09:46:41 +0200

    block5-logging

---
+++ block5-logging/src/main/java/com/block5logging/block5logging/Block5LoggingApplication.java
@@ -0,0 +1,22 @@
+package com.block5logging.block5logging;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class Block5LoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Block5LoggingApplication.class, args);
+
+		log.error("Mensaje a nivel de ERROR");
+		log.trace("Mensaje a nivel de TRACE");
+		log.debug("Mensaje a nivel de DEBUG");
+		log.info("Mensaje a nivel de INFO");
+		log.warn("Mensaje a nivel de WARNING");
+		log.error("Segundo Mensaje a nivel de ERROR");
+	}
+
+}
